1673c9c5d2b85187516f47a569d29bd7b511bcfc
Soumya296 <63197581+Soumya296@users.noreply.github.com>
2020-08-29 01:47:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import h5py
 from tensorflow import keras
-lst1 = np.range(0,24)
+lst1 = [0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 lst2 = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y']
 dct = {zip(lst1,lst2)}
 
